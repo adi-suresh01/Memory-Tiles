26de6758b4a382a8a5431da9cefe397d201defe1
adi-suresh01 <152752311+adi-suresh01@users.noreply.github.com>
2025-02-19 14:31:59 -0500

    difficultyview ui edits

--- Memory Tiles/Difficulty.swift
+++ Memory Tiles/Difficulty.swift
@@ -19,31 +19,58 @@ struct DifficultyView: View {
             VStack {
                 Text("Select Difficulty")
                     .font(.title)
-                    .padding()
+                    .padding(.top, 20)
                 
                 NavigationLink(destination: GameView(selectedImage: selectedImage, gridSize: 4)) {
                     Text("Easy (4x4)")
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+//                        .padding()
+//                        .background(Color.blue)
+//                        .foregroundColor(.white)
+//                        .cornerRadius(10)
                 }
+                .buttonStyle(
+                    PuzzleButtonStyle(
+                             backgroundColor: .blue.opacity(0.8),
+                             cornerRadius: 0,
+                             arcRadius: 25,
+                             caveDepth: 36,
+                             protrusionDepth: 36
+                             )
+                    )
                 
                 NavigationLink(destination: GameView(selectedImage: selectedImage, gridSize: 5)) {
                     Text("Medium (5x5)")
-                        .padding()
-                        .background(Color.orange)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+//                        .padding()
+//                        .background(Color.orange)
+//                        .foregroundColor(.white)
+//                        .cornerRadius(10)
                 }
+                .buttonStyle(
+                    PuzzleButtonStyle(
+                             backgroundColor: .orange.opacity(0.8),
+                             cornerRadius: 0,
+                             arcRadius: 25,
+                             caveDepth: 36,
+                             protrusionDepth: 36
+                             )
+                    )
                 
                 NavigationLink(destination: GameView(selectedImage: selectedImage, gridSize: 6)) {
                     Text("Hard (6x6)")
-                        .padding()
-                        .background(Color.red)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+//                        .padding()
+//                        .background(Color.red)
+//                        .foregroundColor(.white)
+//                        .cornerRadius(10)
                 }
+                .buttonStyle(
+                    PuzzleButtonStyle(
+                             backgroundColor: .red.opacity(0.8),
+                             cornerRadius: 0,
+                             arcRadius: 25,
+                             caveDepth: 36,
+                             protrusionDepth: 36
+                             )
+                    )
             }
         }
     }
